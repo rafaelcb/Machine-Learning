3c98e3ce694911731b3657dbcbaa48ea83244a9c
rafaelcb <rcastrillob@gmail.com>
2015-05-24 08:06:51 -0600

    new files

--- Data analysis.R
+++ Data analysis.R
@@ -1,11 +1,13 @@
 setwd("~/data/Machine Learning")
-train<-read.csv("pml-training.csv",na.string=c("#DIV/0!","","NA"))
-test<-read.csv("pml-testing.csv",na.string=c("#DIV/0!","","NA"))
+train<-read.csv("pml-training.csv",na.string=c("#DIV/0!","","NA"),row.names=1)
+test<-read.csv("pml-testing.csv",na.string=c("#DIV/0!","","NA"),row.names=1)
 na<-apply(train,2,function(x){sum(is.na(x))})
 train<-train[,na==0]
+train<-train[,-c(2:7)]
 
 
 library(caret)
-mod1<-train(classe~.,data=train,method="rpart")
+ctrl<-trainControl(method="cv",number=10)
+mod1<-train(classe~.,data=train,method="rf",trControl=ctrl,ntrees=200)
 library(rpart.plot)
 prp(mod1$finalModel)
